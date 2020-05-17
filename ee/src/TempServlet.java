6bd3b8c07fe399989c0926173af55082d2c19b08
Abergaz <zagreba@gmail.com>
2020-05-17 21:53:59 +0300

    Урок 9. XSS - Cross Site Scripting Защита от вставки злоумышленником в параметры различных скриптов.

--- ee/src/TempServlet.java
+++ ee/src/TempServlet.java
@@ -17,52 +17,18 @@ public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /** Получение параметров из адресной строки по имени*/
         String one = req.getParameter("one");
-        String two = req.getParameter("two");
-        System.out.println(one);
-        /**  Если передали несколько параметров с одним именем */
-        String[] ones = req.getParameterValues("one");
-        for (String x : ones) {
-            System.out.println(x);
-        }
-        /** Список имен параметров */
-        Enumeration<String> parameterNames = req.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            System.out.println(paramName + "=" + Arrays.asList(req.getParameterValues(paramName)).toString());
-        }
-        /** получение карты имя параметра - значение */
-        Map<String, String[]> map = req.getParameterMap();
-        for (Map.Entry<String, String[]> entry : map.entrySet()) {
-            System.out.print(entry.getKey() + "=");
-            for (String x : entry.getValue()) {
-                System.out.print(x + "; ");
-            }
-            System.out.println();
-        }
-        /** куча параметров сервлета */
-        System.out.println(req.getRequestURL());
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getRequestedSessionId());
-        System.out.println(req.getServletPath());
-        System.out.println(req.getServerName());
-        System.out.println(req.getServerPort());
-        System.out.println(req.getRemoteHost());
-        System.out.println(req.getRemotePort());
-        System.out.println(req.getRemoteUser());
-
-        /** получение строки запроса */
-        System.out.println(req.getQueryString());
-
-
+        /** Для того чтобы злоумышленик не мог вставить javascript или тругой код
+         * необходимо экранировать разные символы на HTML символы &
+         * просто replace или использовать специальные библиотеки*/
+        one = one == null ? "" : one.replaceAll("<", "&lt;").replaceAll(">","&gt;");
         /**  Используем для отправки формы метод  get*/
         resp.getWriter().write("<html>" +
                 "<head></head>" +
                 "<body>" +
-                "<form action='temp' method=get>"+
-                "<input type='text' name='one'/>"+
-                "<input type='text' name='two'/>"+
+                "one=" + one +
+                "<form action='temp' method=post>"+
+                "<textarea name='one'/></textarea>"+
                 "<input type='submit' name='submit'/>"+
                 "</form>"+
                 "</body>"+
@@ -72,24 +38,6 @@ public class TempServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /**  Используем для отправки формы метод  POST */
-        resp.getWriter().write("<html>" +
-                "<head></head>" +
-                "<body>" +
-                "<form action='temp' method=post>"+
-                "<input type='text' name='one'/>"+
-                "<input type='text' name='two'/>"+
-                "<input type='submit' name='submit'/>"+
-                "</form>"+
-                "</body>"+
-                "</html>"
-        );
-        /** параметры для пост запросов получаются также как и для get */
-        /** Список имен параметров */
-        Enumeration<String> parameterNames = req.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            System.out.println(paramName + "=" + Arrays.asList(req.getParameterValues(paramName)).toString());
-        }
+       doGet(req,resp);
     }
 }
