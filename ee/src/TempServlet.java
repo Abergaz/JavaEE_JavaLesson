b4f82fb1bc2b4ed1b4890a632f2f436cbc9ee031
Abergaz <zagreba@gmail.com>
2020-05-17 20:14:07 +0300

    Урок 8. URL параметры

--- ee/src/TempServlet.java
+++ ee/src/TempServlet.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
@@ -14,32 +17,79 @@ public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        synchronized (this) {
-            for (int j = 0; j < 1_000_000; j++) {
-                i++;
-            }
-            System.out.println(i);
-            System.out.println(Thread.currentThread().getName());
+        /** Получение параметров из адресной строки по имени*/
+        String one = req.getParameter("one");
+        String two = req.getParameter("two");
+        System.out.println(one);
+        /**  Если передали несколько параметров с одним именем */
+        String[] ones = req.getParameterValues("one");
+        for (String x : ones) {
+            System.out.println(x);
         }
+        /** Список имен параметров */
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            System.out.println(paramName + "=" + Arrays.asList(req.getParameterValues(paramName)).toString());
+        }
+        /** получение карты имя параметра - значение */
+        Map<String, String[]> map = req.getParameterMap();
+        for (Map.Entry<String, String[]> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + "=");
+            for (String x : entry.getValue()) {
+                System.out.print(x + "; ");
+            }
+            System.out.println();
+        }
+        /** куча параметров сервлета */
+        System.out.println(req.getRequestURL());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getRequestedSessionId());
+        System.out.println(req.getServletPath());
+        System.out.println(req.getServerName());
+        System.out.println(req.getServerPort());
+        System.out.println(req.getRemoteHost());
+        System.out.println(req.getRemotePort());
+        System.out.println(req.getRemoteUser());
+
+        /** получение строки запроса */
+        System.out.println(req.getQueryString());
+
+
+        /**  Используем для отправки формы метод  get*/
+        resp.getWriter().write("<html>" +
+                "<head></head>" +
+                "<body>" +
+                "<form action='temp' method=get>"+
+                "<input type='text' name='one'/>"+
+                "<input type='text' name='two'/>"+
+                "<input type='submit' name='submit'/>"+
+                "</form>"+
+                "</body>"+
+                "</html>"
+        );
     }
 
-
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            new Thread() {
-                @Override
-                public void run() {
-                    URLConnection urlConnection = null;
-                    try {
-                        urlConnection = new URL("http://localhost:8080/temp").openConnection();
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /**  Используем для отправки формы метод  POST */
+        resp.getWriter().write("<html>" +
+                "<head></head>" +
+                "<body>" +
+                "<form action='temp' method=post>"+
+                "<input type='text' name='one'/>"+
+                "<input type='text' name='two'/>"+
+                "<input type='submit' name='submit'/>"+
+                "</form>"+
+                "</body>"+
+                "</html>"
+        );
+        /** параметры для пост запросов получаются также как и для get */
+        /** Список имен параметров */
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            System.out.println(paramName + "=" + Arrays.asList(req.getParameterValues(paramName)).toString());
         }
     }
 }
