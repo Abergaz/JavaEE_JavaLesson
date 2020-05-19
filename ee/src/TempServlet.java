6f9620b8e84af877e897d71e284784b7a683ebf3
Abergaz <zagreba@gmail.com>
2020-05-19 10:49:32 +0300

    Урок 14. Сессия HttpSession httpSession = req.getSession();

--- ee/src/TempServlet.java
+++ ee/src/TempServlet.java
@@ -1,34 +1,29 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Enumeration;
 
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /** Создаем куки */
-        Cookie cookie = new Cookie("name","value");
-        /** сроки хранения куки -1 бесконечность по умолчанию */
-        cookie.setMaxAge(5);
-        /** доступ к куки можно ограничить только конкретной страницей setPath */
-        cookie.setPath("/temp");
-        /** ограничть доступ к куки только по SSL setSecure */
-        cookie.setSecure(true);
-        /** куки обязательно записываем в ответ */
-        resp.addCookie(cookie);
-
-        /** Читаем все куки */
-        Cookie[] cookies = req.getCookies();
-        if (cookies!=null){
-            for (Cookie c:cookies){
-                System.out.println(c.getName() + ": "+c.getValue());
-            }
+       /** Сессия хранится на сервере и получаем только из запроса */
+        HttpSession httpSession = req.getSession();
+       /** Пишем атрибут в сессию */
+       httpSession.setAttribute("nameAttribute","valueAttribute");
+       /** Получаем все атрибуты из сессии */
+        Enumeration<String> attributes = httpSession.getAttributeNames();
+        while (attributes.hasMoreElements()){
+            String name = attributes.nextElement();
+            System.out.println(name +": "+httpSession.getAttribute(name));
         }
+        /** Время жизни сессии задается на сервере по умолчанию или в коде */
+        System.out.println(httpSession.getMaxInactiveInterval());
 
     }
 }
