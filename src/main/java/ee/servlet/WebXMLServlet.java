653cd22504839af3438636dddfe6b0637e90846c
Abergaz <zagreba@gmail.com>
2020-05-20 20:21:52 +0300

    Урок 30. Инициализация сервлета и JSP в web.xml можно описать параметры которые будут доступны в сервлете через getServletConfig().getInitParameter(name) <init-param>             <param-name>age</param-name>             <param-value>25</param-value> </init-param>
    Можно указать порядок инициализации севлетов, начинается с 0
     <load-on-startup>0</load-on-startup>
    
     Также есть глобальные параметры, доступные во всех сервлетах и JSP
     context-param>
             <param-name>email</param-name>
             <param-value>mail@gmail.com</param-value>
     </context-param>

--- src/main/java/ee/servlet/WebXMLServlet.java
+++ src/main/java/ee/servlet/WebXMLServlet.java
@@ -6,12 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /** вместо @WebServlet используем описание сервлета в web.xml */
 
 public class WebXMLServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World");
+       /** получаем параметры описанные в web.xml*/
+        resp.getWriter().write(getServletConfig().getInitParameter("name"));
+       /** или вывести все */
+        Enumeration<String> initParameterNames =  getServletConfig().getInitParameterNames();
+        while (initParameterNames.hasMoreElements()){
+            String name = initParameterNames.nextElement();
+            System.out.println(name+": "+ getServletConfig().getInitParameter(name));
+        }
+        /** получем context-param обшие для всех сервлетов */
+        System.out.println(getServletContext().getInitParameter("email"));
+
     }
 }
