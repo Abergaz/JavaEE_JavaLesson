5947ac992adf11c905802916a712886083f8f148
Abergaz <zagreba@gmail.com>
2020-05-22 19:38:28 +0300

    Урок 49. Disposes метод помеченный @Disposes вызовется в самом конце при очистке ресурсов

--- src/main/java/ee/servlet/DependencyInjectionExample.java
+++ src/main/java/ee/servlet/DependencyInjectionExample.java
@@ -2,6 +2,7 @@ package ee.servlet;
 
 import javax.ejb.EJB;
 import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Qualifier;
@@ -18,63 +19,37 @@ import java.lang.annotation.Target;
 
 
 @WebServlet("/DIExample")
-public class DependencyInjectionExample  extends HttpServlet {
-    @Inject
-    String s;
-
-    @Inject @S2
-    String s2;
-
-
-    @Inject
-    int i;
-
-    @Inject
-    double d;
-
+public class DependencyInjectionExample extends HttpServlet {
     @Inject
     Car car;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("s="+s+"; ");
-        resp.getWriter().write("i"+i+"; ");
-        resp.getWriter().write("d="+d+"; ");
-        resp.getWriter().write("car.name="+car.name+"; ");
-        resp.getWriter().write("s2="+s2+"; ");
+        resp.getWriter().write(car.name);
     }
 }
-@Qualifier
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.TYPE,ElementType.METHOD})
-@interface S2{};
 
-class Producer{
+class Producer {
     @Produces
-    String s = "hello world";
-
-    @Produces @S2
-    String s2 = "second string";
-
-    @Produces
-    int i=5;
-
-    @Produces
-    double getDouble(){
-        return 1+3.3+5.8;
-    }
-
-    @Produces
-    Car getCar(){
+    Car getCar() {
         return (new Car("lada"));
     }
-}
-class Car{
-    String name;
 
+    /** метод помеченный @Disposes вызовется в самом конце при очистке ресурсов*/
+    public void clean(@Disposes Car car){
+        car.clean();
+    }
+
+}
+
+class Car {
+    String name;
 
     public Car(String name) {
         this.name = name;
+    }
+    public void clean(){
+        System.out.println("car clean");
     }
 }
 
