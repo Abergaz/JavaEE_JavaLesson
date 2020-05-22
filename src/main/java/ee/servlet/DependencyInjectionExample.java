17e09c4238cc789e61e9b0e7ca6db53450a6752b
Abergaz <zagreba@gmail.com>
2020-05-22 20:29:47 +0300

    Урок 50. InjectionPoint InjectionPoint позволяет получить дополнительную информациб где был заинжектен класс и другую.

--- src/main/java/ee/servlet/DependencyInjectionExample.java
+++ src/main/java/ee/servlet/DependencyInjectionExample.java
@@ -1,56 +1,34 @@
 package ee.servlet;
 
-import javax.ejb.EJB;
-import javax.enterprise.inject.Alternative;
-import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
-import javax.inject.Qualifier;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @WebServlet("/DIExample")
 public class DependencyInjectionExample extends HttpServlet {
     @Inject
-    Car car;
-
+    Logger logger;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write(car.name);
+        logger.log(Level.ALL,"message");
     }
 }
 
-class Producer {
+class MyLogger {
     @Produces
-    Car getCar() {
-        return (new Car("lada"));
+    public Logger getLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
     }
 
-    /** метод помеченный @Disposes вызовется в самом конце при очистке ресурсов*/
-    public void clean(@Disposes Car car){
-        car.clean();
-    }
-
-}
-
-class Car {
-    String name;
-
-    public Car(String name) {
-        this.name = name;
-    }
-    public void clean(){
-        System.out.println("car clean");
-    }
 }
 
 
