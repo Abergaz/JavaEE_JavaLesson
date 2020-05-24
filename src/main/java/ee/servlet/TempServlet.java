dbe899291c5b19c6c209598b5b2b03f2abd19876
Abergaz <zagreba@gmail.com>
2020-05-24 15:46:45 +0300

    Урок 62.Пул потоков и конкуренция Можно инжектить managed усправляемые контейнером пулы потоков, работать  ними и при этом не заботится о их сохдании и заверщшении, эту работу на себя берет контейнер.

--- src/main/java/ee/servlet/TempServlet.java
+++ src/main/java/ee/servlet/TempServlet.java
@@ -1,15 +1,50 @@
 package ee.servlet;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.enterprise.concurrent.ManagedThreadFactory;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
-@WebServlet("/temp")
+@WebServlet("/tempServlet")
 public class TempServlet extends HttpServlet {
+    /** получаем доступ к различным управляемым котейнером пулам потоков*/
+    @Resource
+    ManagedExecutorService managedExecutorService;
+    @Resource
+    ManagedScheduledExecutorService managedScheduledExecutorService;
+    @Resource
+    ManagedThreadFactory managedThreadFactory;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        managedExecutorService.submit(new MyRunnuble());
+        try {
+            System.out.println(managedExecutorService.submit(new MyCallble<>()).get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class MyRunnuble implements Runnable{
+        @Override
+        public void run() {
+            System.out.println("1");
+        }
+    }
+    static class MyCallble<T> implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            return "2";
+        }
     }
 }
