03fe37660573cebcf1a0cb2d5530d567deb151a3
Abergaz <zagreba@gmail.com>
2020-05-24 10:43:59 +0300

    Урок 55. Interceptors Можно создать класс в котором методы пометить аннотациями : @AroundConstruct - будет вызван перед конструктором @PostConstruct - после создания бина дефолтным конструктором @AroundInvoke - перед вызовом методов @PostDestroy - после уничтожения бина
    После этого бин помечеаем @Interceptors и к нему ко всему применяется то что описано в классе интерсепторе. Можно помечать не весь класс а отдельные методы или исключать методы

--- src/main/java/ee/servlet/DependencyInjectionExample.java
+++ src/main/java/ee/servlet/DependencyInjectionExample.java
@@ -5,8 +5,7 @@ import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.interceptor.AroundInvoke;
-import javax.interceptor.InvocationContext;
+import javax.interceptor.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,29 +24,47 @@ public class DependencyInjectionExample extends HttpServlet {
         liveCycleBean.doJob2();
     }
 }
-class LiveCycleBean{
-    public LiveCycleBean() {
-        System.out.println("construct");
+
+class Interceptor{
+    @AroundConstruct/** вызывается перед созданием бина, перед конструктором */
+    private void aroundConstruct(InvocationContext context) throws Exception{
+        System.out.println("before construct");
+        context.proceed();
     }
+
     @PostConstruct /** анотация говорящая что надо вызвать метод сразу после создания обьекта бина */
-    private  void postConstruct(){
+    private  void postConstruct(InvocationContext context) throws Exception{
         System.out.println("post construct");
+        context.proceed();
     }
-    @AroundInvoke /** вызовктся перед каждым методом*/
+    @AroundInvoke /** вызовается перед каждым методом*/
     private Object aroundInvoke(InvocationContext context) throws Exception {
         System.out.println("before method");
         return context.proceed();
     }
     @PreDestroy /** вызовется перед уничтожением бина*/
-    private void preDestroy(){
+    private void preDestroy(InvocationContext context) throws Exception{
         System.out.println("pre destroy");
+        context.proceed();
     }
+}
 
-
+@Interceptors(Interceptor.class) /** помечаем бин что  к нему пименяются интерсептор */
+@RequestScoped
+class LiveCycleBean{
+    public LiveCycleBean() {
+        System.out.println("construct");
+    }
     public void doJob(){
         System.out.println(" do job");
     }
+    @ExcludeClassInterceptors /** значит перед этим методом не будут применяться интерсепторы */
     public void doJob2(){
         System.out.println("do job2");
+    }
+
+    @Interceptors(Interceptor.class)/** можно помечать отдельные методы  а не класс целиком*/
+    public void doJob3(){
+        System.out.println("do job3");
     }
 }
