ea69e5fb9a0d84e35cc1437107b79ce8a7d1baf5
Abergaz <zagreba@gmail.com>
2020-05-24 20:58:38 +0300

    Урок 64. Валидация бинов Это набор аннотация которые можно навешивать на свойства бина или геттеры или параметы. А затем проверять с помощью валидатора Validator нарушения ограничений ConstraintViolation

--- src/main/java/ee/servlet/Temp.java
+++ src/main/java/ee/servlet/Temp.java
@@ -6,18 +6,48 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import javax.validation.constraints.*;
 import java.io.IOException;
-import java.util.Date;
+import java.util.Set;
 
 @WebServlet("/temp")
 public class Temp extends HttpServlet {
     @Inject
     Person person;
-
+//    @Inject
+//    Validation validation;
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("person name = " +person.name);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { resp.getWriter().write("person name = " +person.name);
+        person.name="Max";
+        person.age=15;
+        ValidatorFactory validatorFactory=Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        /** проверяем все условия*/
+        Set<ConstraintViolation<Person>> validate =validator.validate(person);
+        if (validate.size()>0){
+            System.out.println("some error ocured");
+        }
+        for (ConstraintViolation<Person> violation :validate){
+            System.out.println(violation.getMessage());
+            System.out.println(violation.getInvalidValue());
+        }
+        /** проверяем услоия для свойства name*/
+        Set<ConstraintViolation<Person>> name =validator.validateProperty(person,"name");
+        if (name.size()>0){
+            System.out.println("name is wrong");
+        }
+        /** проверяем значение anton на правильность для свойства name класс Person*/
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validateValue(Person.class, "name","anton");
+        if (name.size()>0){
+            System.out.println("anton is wrong");
+        }
+        /** Можно также проверять параметы методов помеченные валидаторами*/
+
     }
 }
 class Person{
@@ -26,12 +56,4 @@ class Person{
     String name;
     @Min(18)
     int age;
-    @Size(min = 10, max = 200)
-    String description;
-    @Past /** гооворит что дада должна быть из прошлого < текущей,
-     @Future - наоборот*/
-    Date birthDate;
-
-    /** эти анатации могут быть или над свойствами или над геттерами
-     * или над параметрами над методами*/
 }
