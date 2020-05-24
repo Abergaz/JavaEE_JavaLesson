0a0c0f2293ce72881842d64676ad5a9edb85acb0
Abergaz <zagreba@gmail.com>
2020-05-24 13:07:45 +0300

    Урок 58. Decorator @Decorator @Delegate

--- src/main/java/ee/servlet/DependencyInjectionExample.java
+++ src/main/java/ee/servlet/DependencyInjectionExample.java
@@ -1,114 +1,44 @@
 package ee.servlet;
 
-import javax.enterprise.context.RequestScoped;
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
 import javax.inject.Inject;
-import javax.interceptor.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @WebServlet("/DIExample")
 public class DependencyInjectionExample extends HttpServlet {
     @Inject
-    InterceptorsService interceptorsService;
-
+    Parent parent;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        interceptorsService.doJob();
-        interceptorsService.doJob2();
+        parent.print();
     }
 }
 
-@InterceptorBinding
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@interface One {
+interface Parent{
+    void print();
 }
-
-@Interceptor
-@One
-class InterceptorOne {
-    @AroundInvoke
-    /** вызовается перед каждым методом*/
-    private Object aroundInvoke(InvocationContext context) throws Exception {
-        System.out.println("one");
-        return context.proceed();
+class Child implements Parent{
+    @Override
+    public void print() {
+        System.out.println("print");
     }
 }
-
-@InterceptorBinding
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@interface Two {
-}
-
-
-@Two
-@Interceptor
-class InterceptorTwo {
-    @AroundInvoke
-    /** вызовается перед каждым методом*/
-    private Object aroundInvoke(InvocationContext context) throws Exception {
-        System.out.println("two");
-        return context.proceed();
-    }
-}
-
-@InterceptorBinding
-@Target({ElementType.TYPE, ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
-@interface Free {
-}
-
-
-@Free
-@Interceptor
-class InterceptorFree {
-    @AroundInvoke
-    /** вызовается перед каждым методом*/
-    private Object aroundInvoke(InvocationContext context) throws Exception {
-        System.out.println("free");
-        return context.proceed();
-    }
-}
-
-@InterceptorBinding
-@Target({ElementType.TYPE, ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
-@interface Four {
-}
-
-
-@Four
-@Interceptor
-class InterceptorFour {
-    @AroundInvoke
-    /** вызовается перед каждым методом*/
-    private Object aroundInvoke(InvocationContext context) throws Exception {
-        System.out.println("four");
-        return context.proceed();
-    }
-}
-
-@One
-@Two
-@RequestScoped
-class InterceptorsService {
-    @Free
-    @Four
-    public void doJob() {
-        System.out.println(" do job");
-    }
-
-    public void doJob2() {
-        System.out.println("do job2");
+@Decorator
+class MyDecorator implements Parent{
+    @Inject
+    @Delegate
+    Parent parent;
+    @Override
+    public void print() {
+        System.out.println("before print");
+        parent.print();
+        System.out.println("after print");
     }
 
 }
