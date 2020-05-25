0655dd5c7466afb49b7442c716c5203ad20cae90
Abergaz <zagreba@gmail.com>
2020-05-25 20:47:14 +0300

    Урок 70. Группы валидации У проверочных аннотаций бинов есть параметр групп. Можно создать интерфейсы и ими метить группы проверочных аннотаций и потом проверять только определенные группы

--- src/main/java/ee/servlet/Temp.java
+++ src/main/java/ee/servlet/Temp.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.validation.constraints.NotNull;
+import javax.validation.groups.Default;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Set;
@@ -21,19 +22,30 @@ import java.util.Set;
 public class Temp extends HttpServlet {
    @Inject
     Validator validator;
+   @Inject PersonT personT;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Set<ConstraintViolation<PersonT>> constraintViolationSet = validator.validateValue(PersonT.class,"url","http://host.com:23") ;
+        Set<ConstraintViolation<PersonT>> constraintViolationSet = validator.validate(personT,MyGroup1.class) ;/** завалидируются только параметры помеченыне MyGroup1*/
         for(ConstraintViolation<PersonT> constraintViolation: constraintViolationSet){
             System.out.println(constraintViolation.getMessage());
             System.out.println(constraintViolation.getInvalidValue());
         }
     }
 }
+interface MyGroup1{}
+interface MyGroup2{}
+interface MyGroup3{}
+interface MyGroup4{}
 
 class PersonT{
-    @CheckSiteURL(port = 22)
-    String url;
+    @NotNull(groups = MyGroup1.class)
+    String s;
+    @NotNull(groups = {MyGroup1.class,MyGroup2.class})
+    String s2;
+    @NotNull(groups = MyGroup3.class)
+    String s3;
+    @NotNull(groups = Default.class)/** дефалтовая группа */
+    String s4;
 }
 
