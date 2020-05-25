168329868573dda92fff14d3e646b0c8956f4478
Abergaz <zagreba@gmail.com>
2020-05-25 21:04:04 +0300

    Урок 71. Валидация через XML Можно не указывать валидацию бинов через аннотацию А создать 2 xml файла webap/web-inf/validation.xml в котором создать настройки и указать в каких файлах описаны настройки валидации бинов аля constrains.xml

--- src/main/java/ee/servlet/Temp.java
+++ src/main/java/ee/servlet/Temp.java
@@ -1,8 +1,5 @@
 package ee.servlet;
 
-import ee.beanValidation.CheckEmail;
-import ee.beanValidation.CheckSiteURL;
-
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,10 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.Default;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Set;
 
 
@@ -26,26 +20,16 @@ public class Temp extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Set<ConstraintViolation<PersonT>> constraintViolationSet = validator.validate(personT,MyGroup1.class) ;/** завалидируются только параметры помеченыне MyGroup1*/
+        Set<ConstraintViolation<PersonT>> constraintViolationSet = validator.validate(personT) ;
         for(ConstraintViolation<PersonT> constraintViolation: constraintViolationSet){
             System.out.println(constraintViolation.getMessage());
             System.out.println(constraintViolation.getInvalidValue());
         }
     }
 }
-interface MyGroup1{}
-interface MyGroup2{}
-interface MyGroup3{}
-interface MyGroup4{}
 
 class PersonT{
-    @NotNull(groups = MyGroup1.class)
-    String s;
-    @NotNull(groups = {MyGroup1.class,MyGroup2.class})
-    String s2;
-    @NotNull(groups = MyGroup3.class)
-    String s3;
-    @NotNull(groups = Default.class)/** дефалтовая группа */
-    String s4;
+   String name;
+   int age;
 }
 
