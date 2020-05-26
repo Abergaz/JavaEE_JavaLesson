6f12286f7c08c66d1b52fae5b47c314711441a57
Abergaz <zagreba@gmail.com>
2020-05-26 14:03:16 +0300

    Урок 76. Многопоточность По уполчанию EJB бины потокобезопасны и контролирует это контейнер, если изменения состояний бина делается методами бина. Можно немного эти управлять с помощью доп аннотаций: @Lock(LockType.WRITE) /** управляет потокобезопсностью, блокируется запись, изенение данных бина*/ @AccessTimeout(value = 5,unit = TimeUnit.SECONDS)/** разлочить bean, если он не разлочиться сам чрере 5 секунд */ @ConcurrencyManagement(ConcurrencyManagementType.BEAN)/** по умолчанию, блокировкой управляет контейнер, если bean то мы должны сами контролировать вручную через synchronized и т.д*/

--- src/main/java/ee/ejb/ExampleServlet.java
+++ src/main/java/ee/ejb/ExampleServlet.java
@@ -1,7 +1,6 @@
 package ee.ejb;
 
 import ee.ejb.bean.ExampleBean;
-import ee.ejb.bean.SecondBean;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -10,15 +9,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 @WebServlet("/ejbExample")
 public class ExampleServlet extends HttpServlet {
     @EJB
     ExampleBean exampleBean;
-    @EJB
-    SecondBean secondBean;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /** так работать не будет, т.к. севрлеты не работают в многопоточном режиме
+        resp.getWriter().write(exampleBean.setI(exampleBean.getI() + 1)); */
+        /** для того чтобы работало необходимо увеличение i перенести в bean*/
+        resp.getWriter().write(exampleBean.increaseI());
+        /** тогда 1000 потоков вернет 1000 цыфр, т.к. EJB потокобезопасны их контролирует контуйнер*/
+    }
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> {
+                URLConnection urlConnection = null;
+                try {
+                    urlConnection = new URL("http://localhost:8080/ejbExample").openConnection();
+                    Scanner scanner = new Scanner(urlConnection.getInputStream());
+                    while (scanner.hasNext()) {
+                        System.out.println(scanner.next());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();;
+        }
     }
 }
--- src/main/java/ee/ejb/bean/ExampleBean.java
+++ src/main/java/ee/ejb/bean/ExampleBean.java
@@ -1,17 +1,24 @@
 package ee.ejb.bean;
 
-
-import javax.annotation.PostConstruct;
-import javax.ejb.DependsOn;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.*;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
-@Startup /** анатация для сингелтонов, чтобы заскадся сразу при старте контейнера*/
-@DependsOn ("SecondBean") /** так сначала запуститься SecondBean потом текущий ExampleBean*/
+@Lock(LockType.WRITE) /** управляет потокобезопсностью, блокируется запись, изенение данных бина*/
+@AccessTimeout(value = 5,unit = TimeUnit.SECONDS)/** разлочить bean, если он не разлочиться сам чрере 5 секунд */
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)/** по умолчанию, блокировкой управляет контейнер, если bean то мы должны сами контролировать вручную через synchronized и т.д*/
 public class ExampleBean{
-    @PostConstruct
-    void postConstruct(){
-        System.out.println("Example bean");
+    int i;
+
+    @Lock(LockType.READ) /** разрешаем этому методу чтение при многопоточности*/
+    public int getI() {
+        return i;
+    }
+
+    public int setI(int i) {
+        return this.i = i;
+    }
+    public int increaseI(){
+        return ++i;
     }
 }
