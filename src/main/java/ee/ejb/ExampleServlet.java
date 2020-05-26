da3acc638f2e3e582a3cfb2a7de0beda99b710b1
Abergaz <zagreba@gmail.com>
2020-05-26 17:24:13 +0300

    Урок 82. Жизненый цикл EJB бина

--- src/main/java/ee/ejb/ExampleServlet.java
+++ src/main/java/ee/ejb/ExampleServlet.java
@@ -3,8 +3,6 @@ package ee.ejb;
 import ee.ejb.bean.ExampleBean;
 
 import javax.ejb.EJB;
-import javax.ejb.embeddable.EJBContainer;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,14 +17,5 @@ public class ExampleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write(exampleBean.getName());
-    }
-
-    public static void main(String[] args) {
-        try(EJBContainer ejbContainer = new EJBContainer.createEJBContainer()){
-            ExampleBean exampleBean = (ExampleBean) ejbContainer.getContext().lookup("java:app/ExampleBean");
-            System.out.println(exampleBean.getName());
-        }catch (NamingException e){
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/ee/ejb/bean/ExampleBean.java
+++ src/main/java/ee/ejb/bean/ExampleBean.java
@@ -1,10 +1,42 @@
 package ee.ejb.bean;
 
-import javax.ejb.Stateless;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
 
-@Stateless
+
+
+/** @Singleton и @Stateless одинаковый жизненый цикл */
+@Stateful /**@Stateful к жизненному циклу добавляется еще 3 метода @PrePassivate и @PostActivate и @Remove*/
 public class ExampleBean{
     public String getName(){
        return "Max ";
    }
+   @PostConstruct
+    void postConstruct(){
+       System.out.println("PostConstruct");
+   }
+    @PreDestroy
+    void preDestroy(){
+        System.out.println("PreDestroy");
+    }
+
+    /** @Stateful к жизненному циклу добавляется еще 3 метода @PrePassivate и @PostActivate и @Remove*/
+
+    @PrePassivate /** вызывается перед сохраенинием */
+    void prePassivate(){
+        System.out.println("PrePassivate");
+    }
+    @PostActivate  /** вызывается после востановления */
+    void postActivate(){
+        System.out.println("postActivate");
+    }
+    @Remove
+    void remove(){
+        System.out.println("remove");
+    }
+
 }
