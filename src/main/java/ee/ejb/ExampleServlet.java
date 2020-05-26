316d82780b52dd77e8bb7510f97631b7edda00e2
Abergaz <zagreba@gmail.com>
2020-05-26 16:39:51 +0300

    Урок 80. JNDI получение EJB Bean Получть EJB bean можно не только через @EJB или @Inject(не рекоменд) А такжк @Resource(lookup=JDNI имя) Или через InitialContext.lookup(JNDI имя)

--- src/main/java/ee/ejb/ExampleServlet.java
+++ src/main/java/ee/ejb/ExampleServlet.java
@@ -2,7 +2,9 @@ package ee.ejb;
 
 import ee.ejb.bean.ExampleBean;
 
-import javax.ejb.EJB;
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,15 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-
 @WebServlet("/ejbExample")
 public class ExampleServlet extends HttpServlet {
-    @EJB
+    @Resource(lookup = "java:app/ExampleBean") /** Можно получить EJB бин через resource указав JNDI имяы*/
     ExampleBean exampleBean;
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       /** или вместо @EJB или @Resource EJB бины можно поолучить из InitialContext использую JNDI имя формата:
+        java:<scope>[/app-name]/[module-name]/bean-name[!<fully-qualified-interface-name] */
+        InitialContext initialContext = null;
+        ExampleBean exampleBean2 = null;
+        try {
+            initialContext = new InitialContext();
+            exampleBean2 = (ExampleBean) initialContext.lookup("java:app/ExampleBean");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
         resp.getWriter().write(exampleBean.getName());
+        resp.getWriter().write(exampleBean2.getName());
     }
 }
--- src/main/java/ee/ejb/bean/ExampleBean.java
+++ src/main/java/ee/ejb/bean/ExampleBean.java
@@ -1,11 +1,10 @@
 package ee.ejb.bean;
 
-import javax.annotation.Resource;
-/** настройка бина описана в web/WEB-INF/ejb-jar.xml*/
+import javax.ejb.Stateless;
+
+@Stateless
 public class ExampleBean{
-    @Resource /** инжектим значение из xml настройки бина*/
-    Double currencyRate;
-   public String getName(){
-       return "Max " +currencyRate;
+    public String getName(){
+       return "Max ";
    }
 }
