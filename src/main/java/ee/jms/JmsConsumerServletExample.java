936c437ac11bebd507759fa35b0d5354c685ad8e
Abergaz <zagreba@gmail.com>
2020-05-31 14:35:23 +0300

    Урок 95. Inject ресурсов JMS Инжектим ConnectionFactory и Queue через @Resource Можно не указывать ссылку на ConnectionFactory тогда том кат сам его создаст.

--- src/main/java/ee/jms/JmsConsumerServletExample.java
+++ src/main/java/ee/jms/JmsConsumerServletExample.java
@@ -1,5 +1,6 @@
 package ee.jms;
 
+import javax.annotation.Resource;
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -9,16 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 @WebServlet("/jmsConsumerServletExample")
 public class JmsConsumerServletExample extends HttpServlet {
+    @Resource(lookup ="openejb:Resource/MyJMSConnectionFactory")
+    ConnectionFactory connectionFactory;
+    @Resource(lookup ="openejb:Resource/FooQueue")
+    Queue queue;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            InitialContext initialContext = new InitialContext();
-            ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("openejb:Resource/MyJMSConnectionFactory");
-            Queue queue = (Queue) initialContext.lookup("openejb:Resource/FooQueue");
             Connection connection = connectionFactory.createConnection();
             connection.start();
             Session session = connection.createSession();
@@ -27,8 +28,6 @@ public class JmsConsumerServletExample extends HttpServlet {
             System.out.println(textMessage.getText());
             resp.getWriter().write(textMessage.getText());
             connection.close();
-        } catch (NamingException e) {
-            e.printStackTrace();
         } catch (JMSException e) {
             e.printStackTrace();
         }
--- src/main/java/ee/jms/JmsProducerServletExample.java
+++ src/main/java/ee/jms/JmsProducerServletExample.java
@@ -1,5 +1,7 @@
 package ee.jms;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -13,20 +15,20 @@ import java.util.Date;
 
 @WebServlet("/jmsProducerServletExample")
 public class JmsProducerServletExample extends HttpServlet {
+    @Resource(lookup ="openejb:Resource/MyJMSConnectionFactory")
+    ConnectionFactory connectionFactory;
+    @Resource(lookup ="openejb:Resource/FooQueue")
+    Queue queue;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            InitialContext initialContext = new InitialContext();
-            ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("openejb:Resource/MyJMSConnectionFactory");
-            Queue queue = (Queue) initialContext.lookup("openejb:Resource/FooQueue");
             Connection connection = connectionFactory.createConnection();
             connection.start();
             Session session = connection.createSession();
             MessageProducer messageProducer = session.createProducer(queue);
             Message message = session.createTextMessage("message was sent at: " + new Date());
+            messageProducer.send(message);
             connection.close();
-        } catch (NamingException e) {
-            e.printStackTrace();
         } catch (JMSException e) {
             e.printStackTrace();
         }
