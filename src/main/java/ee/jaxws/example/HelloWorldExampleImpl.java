6078e447044bd5abe61ea2f7d9bc5bd91b816e2b
Abergaz <zagreba@gmail.com>
2020-06-04 22:03:02 +0300

    Урок 106. Настройка WSDL Можно менять некоторые параметры и имена в WSDL используя параметры в аннотации @WebService и дополнительные аннатоции с параметрами.

--- src/main/java/ee/jaxws/example/HelloWorldExampleImpl.java
+++ src/main/java/ee/jaxws/example/HelloWorldExampleImpl.java
@@ -1,12 +1,25 @@
 package ee.jaxws.example;
 
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.WebServiceContext;
+
 /** пометили класс как web-сервис */
 @WebService (endpointInterface = "ee.jaxws.example.HelloWorldExample",serviceName = "sayHelloService") /** говорим какок интефейс имлементим */
+@SOAPBinding(style = SOAPBinding.Style.RPC, use = SOAPBinding.Use.LITERAL)
 public class HelloWorldExampleImpl implements HelloWorldExample{
+    @Resource
+    WebServiceContext webServiceContext; /** есть полезные методы и свойства */
+    @WebMethod(exclude = true) /** исключает метод из сервиса*/
+    /** @WebMethod(operationName = "newName")  меняем имя*/
+    /** @WebResult(name = "resName") имя возвращаемоего параметра если возвращаестя*/
     public String seyHello(){
         return "Hello world";
+
     }
 }
 
