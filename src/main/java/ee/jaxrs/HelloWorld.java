a453b73d01f2fa503df6349e8ee0498a41135a70
Abergaz <zagreba@gmail.com>
2020-06-06 12:27:40 +0300

    Урок 114. Выброс исключений в REST сервисах

--- src/main/java/ee/jaxrs/HelloWorld.java
+++ src/main/java/ee/jaxrs/HelloWorld.java
@@ -1,30 +1,39 @@
 package ee.jaxrs;
 
+import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import javax.ws.rs.ext.Providers;
+import java.io.IOException;
 
 @Path("/sayHelloWorld")
 @Produces(MediaType.TEXT_PLAIN)
 public class HelloWorld {
-    @Context
-    UriInfo uriInfo; /** можно инжекстить информация о URL*/
-
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
-    public String sayHello(@Context HttpHeaders httpHeaders, /** можно инжектить заголовки */
-                           @Context Request request, /** инжектим запрос*/
-                           @Context Providers providers, /** для преобразования форматов*/
-                           @Context SecurityContext securityContext
-                           ){
-        return uriInfo.getAbsolutePath().toString();
+    public String sayHello(){
+        /** так делать нельзя, т.к. будет выкинут стектрей, это не красиво
+         надо или использовать мапперы*/
+        throw new RuntimeException("RuntimeException");
+        /** или  выкидывать специальные статусы а ошибку логировать */
+        throw new BadRequestException("сообщение"); /** вернет 400 ошибку и сообщение*/
+
+
     }
     @POST
     @Produces("text/plain")
-    public String put(){
-        return "put";
+    public String put() throws IOException {
+        throw new IOException("IOException");
+    }
+}
+/** Маппер ошибок на статус ответа*/
+@Provider
+class RunTimeExceptionMapper implements ExceptionMapper<RuntimeException>{
+    @Override
+    public Response toResponse(RuntimeException exception) {
+        return Response.status(404).entity(exception.getMessage()).type(MediaType.TEXT_PLAIN).build();
     }
 }
