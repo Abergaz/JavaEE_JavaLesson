c7ffdbc89e6e8b45350e54c9fbdffc007702afc7
Abergaz <zagreba@gmail.com>
2020-06-06 17:04:31 +0300

    Урок 119. Жизненные цыклд Websocket @OnOpen @OnMessage - 3 вида @OnError @OnClose

---
+++ src/main/java/ee/websocets/MessageWebSocket.java
@@ -0,0 +1,40 @@
+package ee.websocets;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+@ServerEndpoint("/messageWebSocket")
+public class MessageWebSocket {
+
+    @OnOpen
+    public void OnOpen(Session session){
+        System.out.println("Connection open on server");
+    }
+
+    @OnMessage
+    public void OnMessage(Session session, String message) throws IOException {
+        session.getBasicRemote().sendText("you have send: "+message);
+    }
+
+    @OnMessage
+    public void OnMessageBinary(Session session, ByteBuffer byteBuffer) throws IOException {
+        session.getBasicRemote().sendBinary(byteBuffer);
+    }
+
+    @OnMessage
+    public void OnMessagePing(Session session, PongMessage pongMessage) throws IOException {
+    }
+
+    @OnError
+    public void OnError(Session session, Throwable throwable){
+        throwable.printStackTrace();
+    }
+
+    @OnClose
+    public void OnError(){
+        System.out.println("Server connection is closed");
+    }
+}
+
--- src/main/java/ee/websocets/MyClient.java
+++ src/main/java/ee/websocets/MyClient.java
@@ -3,17 +3,41 @@ package ee.websocets;
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.ByteBuffer;
 
 @ClientEndpoint /** создаем клиент websocket*/
 public class MyClient {
-    @OnMessage /** получаем сообщение от websocket*/
-    public void onMessage(Session session, String message){
-        System.out.println("client get message: " + message);
+    @OnOpen
+    public void OnOpen(Session session){
+        System.out.println("Connection open on client");
+    }
+
+    @OnMessage
+    public void OnMessage(Session session, String message) throws IOException {
+        session.getBasicRemote().sendText("you have send to client: "+message);
+    }
+
+    @OnMessage
+    public void OnMessageBinary(Session session, ByteBuffer byteBuffer) throws IOException {
+        session.getBasicRemote().sendBinary(byteBuffer);
+    }
+
+
+    @OnError
+    public void OnError(Session session, Throwable throwable){
+        throwable.printStackTrace();
+    }
+
+    @OnClose
+    public void OnError(){
+        System.out.println("Server connection is closed");
     }
 
     public static void main(String[] args) throws IOException, DeploymentException {
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         Session session = container.connectToServer(MyClient.class, URI.create("ws://localhost:8080/myEndpoint"));
         session.getBasicRemote().sendText("hello world websocket (endpoint)"); /** отправляем сообщение на websocket*/
+        session.getBasicRemote().sendBinary(ByteBuffer.wrap("hello".getBytes()));
+        session.getBasicRemote().sendPing(ByteBuffer.wrap("hello".getBytes()));
     }
 }
