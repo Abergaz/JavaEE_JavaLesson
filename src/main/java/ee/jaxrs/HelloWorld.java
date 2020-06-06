3feb16c09bce30abaad0e14f93d39d8b26fdf67c
Abergaz <zagreba@gmail.com>
2020-06-06 11:09:03 +0300

    Урок 111. Возвращаемые типы REST Рест сервисе могут возвращать огромное разнообразие типов @Produces(MediaType. - выбрать тип возвращаемого значения это и текст и html и xml и json  и т.д.

--- src/main/java/ee/jaxrs/HelloWorld.java
+++ src/main/java/ee/jaxrs/HelloWorld.java
@@ -1,8 +1,10 @@
 package ee.jaxrs;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/sayHelloWorld") /** помечаем что это RESTfull-сервис и указываем точку входа*/
+@Produces(MediaType.TEXT_PLAIN) /** на все методы тоже самое что и "text/plain" */
 public class HelloWorld {
     @GET /** помечаем метод аннотацией @GET-получить, @PUT-создать, @POST-обновить, @DELETE, @HEAD, @OPTION */
     @Produces("text/plain") /** указываем тип возвращаемого значения - mime type */
