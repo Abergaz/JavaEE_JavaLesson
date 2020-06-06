53cec8feb12423434a4634ceb870081924d70ed8
Abergaz <zagreba@gmail.com>
2020-06-06 20:02:52 +0300

    Урок 124. комады в JSF если пометить класс @ManagedBean из import javax.faces.bean.ManagedBean; то в jsf можно изать этот бин через #{}

---
+++ src/main/java/ee/jsf/PrintHello.java
@@ -0,0 +1,11 @@
+package ee.jsf;
+
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class PrintHello {
+    public void printHello(){
+        System.out.println("print Hello");
+    }
+}
